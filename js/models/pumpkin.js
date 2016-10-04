8e18a2a29b8eef5a4ec23cf041870f1e0e3b73fd
Jerry Feng <jerry.feng41@gmail.com>
2016-10-04 00:14:07 -0400

    add ghost pumpkin animation

--- js/models/pumpkin.js
+++ js/models/pumpkin.js
@@ -3,6 +3,7 @@ function Pumpkin(_sprite) {
 
     this.sprite.animations.add('off', [2], 0, false);
     this.sprite.animations.add('on', [0, 1], 4, true);
+    this.sprite.animations.add('dead', [4, 5], 4, true);
     this.sprite.animations.play('on');
 
     game.physics.arcade.enable(this.sprite);
