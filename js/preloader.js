66df75d3d19248b01da3d612cc035527658eb952
Judy Chen <judy.chen@uwaterloo.ca>
2016-10-10 00:21:52 -0400

    Revert "Revert "+font""
    This reverts commit 1859b60802142f35db324182fd8a124ab28bb102.

--- js/preloader.js
+++ js/preloader.js
@@ -55,6 +55,9 @@ SpookyGame.Preloader.prototype = {
         game.load.image('melee_hitbox', 'assets/melee_hitbox.gif');
         game.load.image('bullet', 'assets/bullet.gif');
 
+        // number font
+        game.load.image('numpbers', 'assets/ui/numpbers.gif');
+
         // sounds
         game.load.audio('bg', 'assets/sound/graveyard.wav');
 
